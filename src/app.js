5c9b483a1633983089ece21ad0e8f62c426f7a92
caixiaoyun <caixiaoyun@betanovo.com>
2017-08-25 11:59:34 +0800

    Add antd

--- src/app.js
+++ src/app.js
@@ -5,11 +5,81 @@
 'use strict'
 
 import './css/style.css'
-import print from './print'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import { Layout, Menu, Icon } from 'antd'
+const { Header, Footer, Sider, Content } = Layout
 
-console.log('[this is app]')
+class App extends React.Component {
+	render() {
+		return (
+			<Layout>
+				<Sider style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }}>
+					<div className="logo" />
+					<Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+						<Menu.Item key="1">
+							<Icon type="user" />
+							<span className="nav-text">nav 1</span>
+						</Menu.Item>
+						<Menu.Item key="2">
+							<Icon type="video-camera" />
+							<span className="nav-text">nav 2</span>
+						</Menu.Item>
+						<Menu.Item key="3">
+							<Icon type="upload" />
+							<span className="nav-text">nav 3</span>
+						</Menu.Item>
+						<Menu.Item key="4">
+							<Icon type="bar-chart" />
+							<span className="nav-text">nav 4</span>
+						</Menu.Item>
+						<Menu.Item key="5">
+							<Icon type="cloud-o" />
+							<span className="nav-text">nav 5</span>
+						</Menu.Item>
+						<Menu.Item key="6">
+							<Icon type="appstore-o" />
+							<span className="nav-text">nav 6</span>
+						</Menu.Item>
+						<Menu.Item key="7">
+							<Icon type="team" />
+							<span className="nav-text">nav 7</span>
+						</Menu.Item>
+						<Menu.Item key="8">
+							<Icon type="shop" />
+							<span className="nav-text">nav 8</span>
+						</Menu.Item>
+					</Menu>
+				</Sider>
+				<Layout style={{ marginLeft: 200 }}>
+					<Header style={{ background: '#fff', padding: 0 }} />
+					<Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+						<div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
+							...
+							<br />
+							Really
+							<br />...<br />...<br />...<br />
+							long
+							<br />...<br />...<br />...<br />...<br />...<br />...
+							<br />...<br />...<br />...<br />...<br />...<br />...
+							<br />...<br />...<br />...<br />...<br />...<br />...
+							<br />...<br />...<br />...<br />...<br />...<br />...
+							<br />...<br />...<br />...<br />...<br />...<br />...
+							<br />...<br />...<br />...<br />...<br />...<br />...
+							<br />...<br />...<br />...<br />...<br />...<br />
+							content
+						</div>
+					</Content>
+					<Footer style={{ textAlign: 'center' }}>
+						Ant Design ©2016 Created by Ant UED
+					</Footer>
+				</Layout>
+			</Layout>
+		)
+	}
+}
 
-const bank = document.getElementsByClassName('bank')
-bank[0].onclick = print
-
-console.log('[jiangjiang]')
+ReactDOM.render(
+	<App />
+, document.getElementById('app'))
---
+++ src/js/Sider.js
@@ -0,0 +1,8 @@
+/**
+ * @copyright Betanovo Ltd. 2017-present
+ * @author caixiaoyun
+ */
+'use strict'
+
+import React from 'react'
+import {} from 'antd'
